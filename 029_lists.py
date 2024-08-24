afea6ff5261d23ab44abf35f82e1ddc1bd62221b
Andrius Zacharovas <azcharovas@outlook.com>
2024-08-24 23:08:49 +0100

    Python functions

--- 029_lists.py
+++ 029_lists.py
@@ -25,7 +25,7 @@ my_friends = ["Victoria", "Mel", "Melanie", "Emma"]
 
 # @TASK Try making your own here:
 
-your_list = ...
+your_list = [42, "Python", 3.14, "Lists", True]
 print(your_list)
 
 # @TASK and print it out by running:
